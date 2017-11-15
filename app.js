96b18a3954b196b12f03b1e985c601479367011c
Zach Broaddus <zach.broaddus@gmail.com>
2017-11-14 18:19:51 -0600

    complete first half of challenge

--- app.js
+++ app.js
@@ -39,17 +39,55 @@ $(document).ready(function(){
     $(newEvent).find(".event-name").html(eventType);
     $(newEvent).find(".event-time").html(hours + ":" + minutes + ":" + seconds);
 
-    $(".river-content").append(newEvent);
+    $(".river-content").prepend(newEvent);
   }
+
+  var clickCount = 0;
+  var mouseoverCount = 0;
+  var mouseoutCount = 0;
+  var submitCount = 0;
+  var keyupCount = 0;
+  var changeCount = 0;
+  var focusCount = 0;
+  var blurCount = 0;
+  var selectCount = 0;
 
   $("#click-event").on("click", function(event){
     event.preventDefault();
     addEventToList(event);
+    clickCount++;
+    $("#clickCount").text(clickCount);
   });
 
   $("#mouseover").mouseover(function(event){
     event.preventDefault();
     addEventToList(event);
+    mouseoverCount++;
+    $("#mouseoverCount").text(mouseoverCount);
+
+    if (mouseoverCount < 2) {
+      $changeColor = $("#river > :nth-child(2)").children().first();
+      $changeColor.css("background-color", "#e242f4");
+    }
+  });
+
+  $(".submit").submit(function(event){
+    event.preventDefault();
+
+    if ($(".submit form input").first().val() === "") {
+      alert("Input field is blank!");
+    } else {
+      addEventToList(event);
+      submitCount++;
+      $("#submitCount").text(submitCount);
+    }
+  });
+
+  $(".keyup").keyup(function(event){
+    event.preventDefault();
+    addEventToList(event);
+    keyupCount++;
+    $("#keyupCount").text(keyupCount);
   });
 
 });
